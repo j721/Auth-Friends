899ab530200feedff7dd95f7e8443c120b9e6b73
j721 <juliadvu@gmail.com>
2020-04-21 17:20:00 -0500

    fixed syntax error with missing spread operator on this.state.credentials

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,55 +1,55 @@
 import React from 'react';
-import { axiosWithAuth} from '../utils/axiosWithAuth';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-class Login extends React.component{
-    state ={
-        credentials:{
-            username: '',
-            password: ''
-        }
-    };
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
 
-    handleChange = e =>{
-        this.setState({
-            credentials:{
-                this.state.credentials,
-                [e.target.name]:e.target.value
-            }
-        });
-    };
-
-    login = e => {
-        e.preventDefault();
-        axiosWithAuth()
-          .post('/api/login', this.state.credentials)
-          .then(res => {
-            localStorage.setItem('token', JSON.stringify(res.data.payload));
-            this.props.history.push('/protected');
-          })
-          .catch(err => console.log({ err }));
-      };
-
-      render(){
-        return (
-            <div>
-              <form onSubmit={this.login}>
-                <input
-                  type="text"
-                  name="username"
-                  value={this.state.credentials.username}
-                  onChange={this.handleChange}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  value={this.state.credentials.password}
-                  onChange={this.handleChange}
-                />
-                <button>Log in</button>
-              </form>
-            </div>
-          );
-        }
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
       }
-      
-export default Login; 
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', JSON.stringify(res.data.payload));
+        this.props.history.push('/protected');
+      })
+      .catch(err => console.log({ err }));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
