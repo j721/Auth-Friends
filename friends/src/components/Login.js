ca9cbe315854f924169a839b94170c1a4e4d8ce0
j721 <juliadvu@gmail.com>
2020-04-21 16:35:33 -0500

    Still working on Login component

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import { axiosWithAuth} from '../utils/axiosWithAuth';
 
+class Login extends React.component{
+    state ={
+        credentials:{
+            username: '',
+            password: ''
+        }
+    }
+
+
+
+
+
+
+    
+    return(
+    )
+}//end of Login component
+
+export default Login; 
