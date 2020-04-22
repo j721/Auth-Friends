7bca225a6cdb092b9bd44b779f2fe493e24d3fca
j721 <juliadvu@gmail.com>
2020-04-21 21:25:27 -0500

    Changed Login component to function instead of class

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-class Login extends React.Component {
-  state = {
-    credentials: {
-      username: '',
-      password: ''
-    }
-  };
+const Login =props=> {
+
+    const [credentials,setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
 
   handleChange = e => {
-    this.setState({
-      credentials: {
-        ...this.state.credentials,
+    setCredentials({
+        ...credentials,
         [e.target.name]: e.target.value
-      }
     });
   };
 
-  login = e => {
+ login = e => {
     e.preventDefault();
     axiosWithAuth()
       .post('/api/login', this.state.credentials)
@@ -29,27 +27,29 @@ class Login extends React.Component {
       .catch(err => console.log({ err }));
   };
 
-  render() {
+
     return (
       <div>
-        <form onSubmit={this.login}>
+        <form onSubmit={login}>
           <input
             type="text"
             name="username"
-            value={this.state.credentials.username}
-            onChange={this.handleChange}
+            value={credentials.username}
+            onChange={handleChange}
+            placeholder="username"
           />
           <input
             type="password"
             name="password"
-            value={this.state.credentials.password}
-            onChange={this.handleChange}
+            value={credentials.password}
+            onChange={handleChange}
+            placeholder="password"
           />
           <button>Log in</button>
         </form>
       </div>
     );
-  }
+
 }
 
 export default Login;
